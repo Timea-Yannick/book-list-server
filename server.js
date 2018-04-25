74148b5e2f7a25922cdb496c7d2439e9788fc2cc
Yaspesberro <yaspesberro@gmail.com>
2018-04-25 16:42:28 -0700

    Adding code to server.jsapp.get('/api/v1/books', (req, res) => {

--- server.js
+++ server.js
@@ -13,18 +13,25 @@ CLIENT.connect();
 CLIENT.on('error', err => console.error(err));
 
 app.use(cors());
-app.get('/', (req, res) => res.redirect(CLIENT_URL)) ;
+app.use(express.json());
 
+app.get('/', (req, res) => res.redirect(CLIENT_URL)) ;
+app.get('/test', (req, res) => res.send('Hello World!'));
 app.get('/test/*', (req, res) => res.send('no further API routes'));
 
 app.get('/api/v1/books', (req, res) => {
-  client.query('SELECT * FROM books;')
+  client.query(`SELECT * FROM books;`)
   .then(results => res.send(results.rows))
   .catch(console.error);
 });
 
+app.get('/api/v1/books/:id', (req, res) => {
+  client.query(`SELECT * FROM books WHERE book_id = ${req.params.id};`)
+  .then(results => res.send(results.rows))
+  .catch(console.error);
+});
+ 
 app.get('*', (req, res) => res.send('404 service not found'));
-
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
 
 
